193591e3db33860fb4d7c28fa22846b0839166de
Kushuh <geoffroy.vincent@agorastoryverse.com>
2025-12-06 17:41:12 +0100

    chores(eslint): switch to nodelib shared config

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,33 +1,5 @@
-import type { Config } from "prettier";
+import { Prettier } from "@a-novel/nodelib-config";
 
-const config: Config = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  semi: true,
-  singleQuote: false,
-  printWidth: 120,
-  importOrder: [
-    // Test imports are always first.
-    "^#/",
-    // Static assets.
-    "^(.*).(css|yaml|json)$",
-    "^(.*).(svg|png|jpg|jpeg)$",
-    // App imports.
-    "^@/",
-    "^[./]",
-    // A-novel imports.
-    "^@a-novel(/(.*))?$",
-    // Important.
-    "^react(/(.*))?$",
-    "^vite(/(.*))?$",
-    // Other.
-    "<THIRD_PARTY_MODULES>",
-  ],
-  importOrderSeparation: true,
-  htmlWhitespaceSensitivity: "strict",
-  language: "postgresql",
-  paramTypes: `{ numbered: ["?"] }`,
-  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-sql", "prettier-plugin-packagejson"],
-};
-
-export default config;
+export default Prettier({
+  sql: true,
+});
