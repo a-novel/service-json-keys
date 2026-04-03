b166877fa14be58d9350acd1de94803d7ba1782f
Geoffroy Vincent <geoffroy.vincent@agorastoryverse.com>
2026-04-03 18:23:02 +0200

    chore(env): remove `.envrc` (#478)
    * chore(env): remove .envrc
    
    * fix: reviews

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -9,6 +9,14 @@ printf "Exposing GRPC on port %s\n" "${GRPC_PORT}"
 POSTGRES_PORT="${POSTGRES_PORT:="$(node -e 'console.log(await (await import("get-port-please")).getRandomPort())')"}"
 export POSTGRES_PORT
 
+# Development/testing-only dummy key for local environments; do not use in production or any non-local/shared
+# environment.
+export APP_MASTER_KEY="${APP_MASTER_KEY:="fec0681a2f57242211c559ca347721766f8a3acd8ed2e63b36b3768051c702ca"}"
+
 export GRPC_URL="${GRPC_URL:="localhost:${GRPC_PORT}"}"
 export REST_URL="${REST_URL:="http://localhost:${REST_PORT}"}"
+export POSTGRES_USER="${POSTGRES_USER:="postgres"}"
+export POSTGRES_PASSWORD="${POSTGRES_PASSWORD:="postgres"}"
+export POSTGRES_DB="${POSTGRES_DB:="json-keys"}"
+export POSTGRES_HOST="${POSTGRES_HOST:="localhost"}"
 export POSTGRES_DSN="${POSTGRES_DSN:="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable"}"
